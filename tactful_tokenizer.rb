924d3e9d435787ec9cb272fe32786a8efc5c253a
SlyShy <mkbunday@stanford.edu>
2010-03-11 11:43:08 -0600

    Use frozen strings for keys, removed redundant c1 and c2.

--- tactful_tokenizer.rb
+++ tactful_tokenizer.rb
@@ -17,20 +17,18 @@ String.class_eval do
 end
 
 class Model
-    def initialize(feats="feats.mar", lower_words="lower_words.mar", non_abbrs="non_abbrs.mar")
+    def initialize(feats="feats_f.mar", lower_words="lower_words.mar", non_abbrs="non_abbrs.mar")
         @feats, @lower_words, @non_abbrs = [feats, lower_words, non_abbrs].map do |file|
             File.open(file) do |f|
                 Marshal.load(f.read)
             end
         end
-        @p0, @p1 = feats('0,<prior>') ** 4, feats('1,<prior>') ** 4  
+        @p0, @p1 = @feats['0,<prior>'.freeze] ** 4, @feats['1,<prior>'.freeze] ** 4  
     end
 
     # Accessors. Obviously these would be prime candidates for both
     # metaprogramming and for andand, but those are poor for performance.
-    def feats(arr)
-        if t = @feats[arr] then t.to_f end
-    end
+    attr_accessor :feats
     def lower_words(arr)
         if t = @lower_words[arr] then t.to_f end
     end
@@ -46,8 +44,8 @@ class Model
     def classify_single(frag)
         probs = [@p0, @p1]
         frag.features.each do |feat|
-            probs[0] *= (feats("0,#{feat}") or 1)
-            probs[1] *= (feats("1,#{feat}") or 1)
+            probs[0] *= (@feats["0,#{feat}".freeze] or 1)
+            probs[1] *= (@feats["1,#{feat}".freeze] or 1)
         end
         normalize(probs)
         frag.pred = probs[1]
@@ -78,28 +76,28 @@ class Model
             words2, w2 = [], ''
         end
 
-        c1 = w1.gsub(/(^.+?\-)/, '')
-        c2 = w2.gsub(/(\-.+?)$/, '')
+        w1.gsub!(/(^.+?\-)/, '')
+        w2.gsub!(/(\-.+?)$/, '')
 
-        frag.features = ["w1_#{c1}", "w2_#{c2}", "both_#{c1}_#{c2}"]
+        frag.features = ["w1_#{w1}".freeze, "w2_#{w2}".freeze, "both_#{w1}_#{w2}".freeze]
 
-        len1 = [10, c1.gsub(/\W/, '').length].min
+        len1 = [10, w1.gsub(/\W/, '').length].min
 
-        if not c2.empty? and c1.gsub('.', '').is_alphabetic? 
-            frag.features.push "w1length_#{len1}"
+        if not w2.empty? and w1.gsub('.', '').is_alphabetic? 
+            frag.features.push "w1length_#{len1}".freeze
             begin
-                frag.features.push "w1abbr_#{Math.log(1 + model.non_abbrs(c1.chop())).to_i}"
+                frag.features.push "w1abbr_#{Math.log(1 + model.non_abbrs(w1.chop())).to_i}".freeze
             rescue Exception => e
-                frag.features.push "w1abbr_0"
+                frag.features.push "w1abbr_0".freeze
             end
         end
 
-        if not c2.empty? and c2.gsub('.', '').is_alphabetic?
-            frag.features.push "w2cap_#{c2[0].is_upper_case?.to_s.capitalize}"
+        if not w2.empty? and w2.gsub('.', '').is_alphabetic?
+            frag.features.push "w2cap_#{w2[0].is_upper_case?.to_s.capitalize}".freeze
             begin
-                frag.features.push "w2lower_#{Math.log(1 + model.lower_words(c2.downcase)).to_i}"
+                frag.features.push "w2lower_#{Math.log(1 + model.lower_words(w2.downcase)).to_i}".freeze
             rescue Exception => e
-                frag.features.push "w2lower_0"
+                frag.features.push "w2lower_0".freeze
             end
         end
     end
